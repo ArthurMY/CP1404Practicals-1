d2e25f902f5d316dc36d75cb07d351228e99badb
BlackWolf <albert.alvaro@my.jcu.edu.au>
2022-11-28 10:28:43 +0800

    Completed quick_picks

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -6,5 +6,8 @@ for i in range(0, quick_pick_number):
     for j in range(0, 6):
         number = random.randint(1, 45)
         random_numbers.append(number)
-    print(str(random_numbers)[1:-1].replace(",", " "))
+        filtered_numbers = []
+        [filtered_numbers.append(x) for x in random_numbers if x not in filtered_numbers]
+        [filtered_numbers.append(random.randint(1, 45)) for x in random_numbers if len(filtered_numbers) < 6]
+    print(str(filtered_numbers)[1:-1].replace(",", " "))
 
